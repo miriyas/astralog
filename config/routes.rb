59db2b6d1b59174f21e3e723e2026359a1a1496d
MIRiyA <miriya.lee@gmail.com>
2014-05-01 20:48:26 +0900

    #4 본문 에디터 테스트를 위한 임시 show 페이지

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 Astralog::Application.routes.draw do
-	namespace :admin do
-    root :to => "posts#index"
-		
-	  get "logout" => "sessions#destroy", as: "logout"
-	  get "login" => "sessions#new", as: "login"
-    post "login" => "sessions#create"
-	
-  	get "reload_parent_category"	
-		resources :users
-		resources :subjects
-		resources :categories
+
+  # root to: 'main#index'
+  # get '/' => 'main#index'
+
+  get '/:id', to: 'posts#show'
+  resources :posts
+
+  namespace :admin do
+    root :to => 'posts#index'
+    
+    get 'logout' => 'sessions#destroy', as: 'logout'
+    get 'login' => 'sessions#new', as: 'login'
+    post 'login' => 'sessions#create'
+  
+    get 'reload_parent_category'  
+    resources :users
+    resources :subjects
+    resources :categories
     resources :posts
-		resources :comments
-	  resources :users
-	end	
+    resources :comments
+    resources :users
+  end 
 end
